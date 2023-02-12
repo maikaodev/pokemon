b60af447f3689f7cf7669eab5e0d284a233200c9
maikaodev <maikao.dev@gmail.com>
2023-02-12 12:16:21 -0300

    Estilo: Página inicial criada

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,84 @@
 <template>
-  <h1>HOME PAGE</h1>
-  <Counter />
+  <div class="container">
+    <div class="header">
+      <h1>Pokémundo</h1>
+      <p>Aqui você encontra tudos sobre o seu pokémon preferido</p>
+    </div>
+
+    <section class="content">
+      <form @submit.prevent>
+        <input
+          type="text"
+          placeholder="Encontre seu pokémon"
+          aria-label="Encontre seu pokémon"
+          required
+        />
+        <button type="submit">Buscar</button>
+      </form>
+    </section>
+  </div>
 </template>
 <script lang="ts">
-import Counter from "../components/Counter.vue";
 export default {
   name: "Home",
-  components: { Counter },
   data() {
     return {};
   },
 };
 </script>
-<style></style>
+<style lang="scss" scoped>
+.container {
+  text-align: center;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  gap: 32px;
+  .header {
+    width: 80%;
+
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    h1 {
+      font-size: 32px;
+    }
+    p {
+      font-size: 18px;
+      font-weight: 300;
+    }
+  }
+  .content {
+    form {
+      display: flex;
+      gap: 16px;
+      input {
+        padding: 8px 16px;
+        &:focus {
+          outline: none;
+        }
+      }
+      button {
+        background: none;
+        color: white;
+
+        padding: 16px;
+
+        font-weight: 700;
+
+        border: 1px solid red;
+
+        cursor: pointer;
+
+        transition-property: background-color;
+        transition-duration: 0.75s;
+
+        &:hover {
+          background-color: red;
+        }
+      }
+    }
+  }
+}
+</style>
