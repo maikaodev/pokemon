d1cdba69ae0f3ae04c7e65c3e8a9f036c68c4615
maikaodev <maikao.dev@gmail.com>
2023-02-12 16:34:38 -0300

    Feat: Fazendo a req e salvando os dados na store

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -8,24 +8,66 @@
     <section class="content">
       <form @submit.prevent>
         <input
+          v-model="pokeName"
           type="text"
           placeholder="Encontre seu pokémon"
           aria-label="Encontre seu pokémon"
           required
         />
-        <button type="submit">Buscar</button>
+        <button type="submit" @click="findPokemon">Buscar</button>
       </form>
     </section>
   </div>
 </template>
+
 <script lang="ts">
+// Functions - utils
+import { fetchData } from "../utils/fetchData";
+
+// Store
+import { SpeciesOfPokemon } from "../stores/SpeciesOfPokemon";
+
+const storeSpeciesPokemon = SpeciesOfPokemon();
+
+// Ts
+import { DataProps, StatsProps } from "../types/pages";
+
 export default {
   name: "Home",
   data() {
-    return {};
+    return {
+      pokeName: "",
+    };
+  },
+  methods: {
+    async findPokemon() {
+      //
+
+      if (!this.pokeName) return;
+
+      const data: DataProps = await fetchData(
+        `https://pokeapi.co/api/v2/pokemon/${this.pokeName.toLowerCase()}`
+      );
+
+      if (data.error) {
+        return alert(data.message);
+      }
+
+      const stats: StatsProps[] = data.stats;
+      const id: number = data.id;
+
+      this.updatingUrl(id, stats);
+
+      //
+      this.$router.push(`/detalhes/${this.pokeName}`);
+    },
+    updatingUrl(id: number, stats: StatsProps[]) {
+      storeSpeciesPokemon.updatingStats(id, stats);
+    },
   },
 };
 </script>
+
 <style lang="scss" scoped>
 .container {
   text-align: center;
