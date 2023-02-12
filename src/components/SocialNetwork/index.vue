6a8692b2a5df9aecbd3a14229132fe9cd1f86af1
maikaodev <maikao.dev@gmail.com>
2023-02-12 11:52:15 -0300

    Estilo: Componente SocialNetwork criado

---
+++ src/components/SocialNetwork/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <nav>
+    <ul>
+      <li><a href="http://github.com/maikaodev" target="_blank">Github</a></li>
+      <li>
+        <a href="https://www.linkedin.com/in/maikaodev/" target="_blank"
+          >LinkedIn</a
+        >
+      </li>
+      <li>
+        <a href="mailto:maikao.dev@gmail.com" target="_blank"></a>
+        Gmail
+      </li>
+    </ul>
+  </nav>
+</template>
+<script>
+export default {
+  name: "SocialNetwork",
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped>
+nav {
+  height: 100%;
+  margin: auto;
+
+  display: flex;
+
+  align-items: center;
+  justify-content: center;
+  ul {
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+
+    list-style: none;
+
+    margin: 0;
+
+    li {
+      a {
+        transition-property: color;
+        transition-duration: 0.75s;
+        &:hover {
+          color: red;
+        }
+      }
+    }
+  }
+}
+
+$breakpoint-tablet: 768px;
+@media (min-width: $breakpoint-tablet) {
+  nav {
+    ul {
+      width: 500px;
+    }
+  }
+}
+</style>
