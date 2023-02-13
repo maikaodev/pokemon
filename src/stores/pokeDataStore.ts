09e37f8777d75c31aed7ff17042433b5072187fc
maikaodev <maikao.dev@gmail.com>
2023-02-13 11:39:51 -0300

    Feat: Melhorando estrutura da store

--- src/stores/pokeDataStore.ts
+++ src/stores/pokeDataStore.ts
@@ -1,25 +1,31 @@
 import { defineStore } from "pinia";
 // TS
-import { StatsProps } from "../types/pages";
+import { ErrorProps, PokemonProps } from "../types/pages";
+import { fetchData } from "../utils/fetchData";
 
 export const pokeDataStore = defineStore("pokeData", {
   state() {
     return {
-      pokename: "",
       url_default: "https://pokeapi.co/api/v2/pokemon/",
       url_species: "https://pokeapi.co/api/v2/pokemon-species/", //https://pokeapi.co/api/v2/pokemon-species/pikachu/
-      stats: [] as StatsProps[],
-      weight: 0,
-      height: 0,
+      pokemons: {} as Record<string, PokemonProps>,
     };
   },
 
   actions: {
-    setPokename(name: string) {
-      this.pokename = name;
-    },
-    setStats(stats: StatsProps[]) {
-      this.stats = stats;
+    async findOrGetPokemon(name: string): Promise<PokemonProps | ErrorProps> {
+      if (this.pokemons[name]) {
+        return this.pokemons[name];
+      }
+      const pokemonData: PokemonProps | ErrorProps = await fetchData(
+        this.url_default + name
+      );
+
+      if ("error" in pokemonData) return pokemonData;
+
+      this.pokemons[name] = pokemonData;
+
+      return this.pokemons[name];
     },
   },
 });
