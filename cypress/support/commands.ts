73135246fd126fce378da86967fdaf81c680c975
maikaodev <maikao.dev@gmail.com>
2023-02-15 12:57:21 -0300

    Test: Configurando cypress com testing library

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,3 +1,4 @@
+import "@testing-library/cypress/add-commands";
 /// <reference types="cypress" />
 // ***********************************************
 // This example commands.ts shows you how to
@@ -35,3 +36,5 @@
 //     }
 //   }
 // }
+
+export {};
